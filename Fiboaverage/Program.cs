10fd0794d409526bad42433c6b30dfabff561e46
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2022-10-04 22:39:56 +0300

    Update

---
+++ Fiboaverage/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Enter the number of pieces");
+int z =int.Parse(Console.ReadLine());
+Methods m= new Methods();
+m.calculate(z);
+Console.ReadKey();
+
+
+public class Methods
+{
+    int a = 1;
+    int b = 1;
+    int c;
+    int total = 0;
+    public void calculate(int z)
+    {
+        for (int i = 1; i<=z; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            total+=c;
+        }
+        Console.WriteLine("Average of the series:" + total/z);
+    }
+
+}
---
+++ RemoveChar/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Text Giriniz: ");
+var text = Console.ReadLine();
+
+Console.Write("Karakter Indexi Giriniz (0-" + (text.Length - 1) + "): ");
+int index = 0;
+Console.WriteLine(text.Remove(index, 1));
+Console.ReadLine();
