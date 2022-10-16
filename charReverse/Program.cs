e5c302b278dc192c660c317e16b5a78835588f34
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2022-10-17 01:51:18 +0300

    up

---
+++ charReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿string ch = "Merhaba Hello Question";
+string newch = "";
+string letterHolder = "";
+for (int i = 0; i <= ch.Length; i++)
+{
+    if (i == 0)
+    {
+        letterHolder = ch[i].ToString();
+    }
+    else if (i == ch.Length)
+    {
+        newch += letterHolder;
+    }
+    else if (ch[i - 1].ToString() == " ")
+    {
+        continue;
+    }
+    else if (ch[i].ToString() == " ")
+    {
+        newch += letterHolder + " ";
+        letterHolder = ch[i + 1].ToString();
+    }
+    else
+    {
+        newch += ch[i].ToString();
+    }
+}
+Console.WriteLine(newch);
